1c747578d8bb763b61c90da314cfb67842315af6
MichaeGon <lodnix@gmail.com>
2016-09-26 23:23:40 +0900

    last

--- kernel.asm
+++ kernel.asm
@@ -1,33 +1,6 @@
-[org 0]
-[bits 16]
+%include "init.inc"
 
-start:
-    mov ax, cs
-    mov ds, ax
-    xor ax, ax
-    mov ss, ax
-
-    cli
-
-    lgdt [gdtr]
-
-    mov eax, cr0
-    or eax, 0x00000001
-    mov cr0, eax
-
-    jmp $+2
-    nop
-    nop
-
-    db 0x66
-    db 0x67
-    db 0xEA ; jmp
-    dd PM_Start
-    dw SysCodeSelector
-
-
-; -- protected mode --
-
+[org 0x10000]
 [bits 32]
 
 PM_Start:
@@ -42,13 +15,10 @@ PM_Start:
     mov ax, VideoSelector
     mov es, ax
     mov edi, 80*2*10+2*10
-    lea esi, [ds:msgPMode]
+    lea esi, [msgPMode]
     call printf
 
     jmp $
-
-
-; --
 
 printf:
     push eax
@@ -69,42 +39,3 @@ printf_end:
     ret
 
 msgPMode db "We are in Protected Mode", 0
-
-gdtr:
-    dw gdt_end - gdt - 1
-    dd gdt + 0x10000
-
-gdt:
-    dw 0
-    dw 0
-    db 0
-    db 0
-    db 0
-    db 0
-
-SysCodeSelector equ 0x08
-    dw 0xFFFF
-    dw 0x0000
-    db 0x01
-    db 0x9A ; P:1, DPL:0, Code, non-conforming, readable
-    db 0xCF ; G:1, D:1, limit16-19:0xF
-    db 0x00
-
-SysDataSelector equ 0x10
-    dw 0xFFFF
-    dw 0x0000
-    db 0x01
-    db 0x92
-    db 0xCF
-    db 0x00
-
-VideoSelector equ 0x18
-    dw 0xFFFF
-    dw 0x8000
-    db 0x0B
-    db 0x40
-    db 0x00
-
-gdt_end:
-;
-;
