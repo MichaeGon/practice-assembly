15958ad5df5c11c3e01d71cc465ddcfe7eb06727
MichaeGon <lodnix@gmail.com>
2017-01-18 23:04:14 +0900

    boot

---
+++ boot.asm
@@ -0,0 +1,43 @@
+[org 0]
+[bits 16]
+
+    jmp 0x07c0:start
+
+start:
+    mov ax, cs
+    mov ds, ax
+
+    mov ax, 0xb800
+    mov es, ax
+
+    mov edi, 0xa0
+    lea esi, [msgRealMode]
+    call printf
+    hlt
+
+printf:
+    push eax
+
+printf_loop:
+    mov al, [esi]
+    or al, al
+    jz printf_end
+
+    mov byte [es:edi], al
+    inc esi
+    inc edi
+    mov byte [es:edi], 0x06
+    inc edi
+
+    jmp printf_loop
+
+printf_end:
+    pop eax
+    ret
+
+msgRealMode db "Real Mode", 0
+
+; end
+    times 510 - ($ - $$) db 0
+    db 0x55
+    db 0xaa
